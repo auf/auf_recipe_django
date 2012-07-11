7b0d690239d23b90ea8421f0eebd3dbd42e98aa9
Olivier Larchevêque <olivier.larcheveque@auf.org>
2012-07-11 10:07:57 -0400

    v2.0 : plus de squelette projet

--- auf/recipe/django/recipe.py
+++ auf/recipe/django/recipe.py
@@ -1,17 +1,9 @@
 # -*- encoding: utf-8 -*-
 
 import os
-import shutil
-import pkg_resources
-import djangorecipe
-from djangorecipe.boilerplate import versions
+import zc
 from djangorecipe.recipe import Recipe as OriginalDjangoRecipe
-from boilerplate import *
-
-djangorecipe.boilerplate.versions['1.3']['settings'] = auf_settings_template
-djangorecipe.boilerplate.versions['1.3']['urls'] = auf_urls_template
-djangorecipe.boilerplate.versions['1.3']['production_settings'] = auf_production_settings
-djangorecipe.boilerplate.versions['1.3']['development_settings'] = auf_development_settings
+from boilerplate import auf_buildout_file, auf_script_template
 
 
 class Recipe(OriginalDjangoRecipe):
@@ -51,19 +43,11 @@ class Recipe(OriginalDjangoRecipe):
 
         return script_paths + [location]
 
-    def create_project(self, project_dir):
-        super(Recipe, self).create_project(project_dir)
-
-        # fichier de configuration de base de données
-        self.create_file(os.path.join(project_dir, 'conf.py'), conf_file, self.options)
-        self.create_file(os.path.join(project_dir, 'conf.py.edit'), conf_file, self.options)
-        self.create_file(os.path.join(project_dir, 'dashboard.py'), dashboard_file, self.options)
-
     def create_manage_script(self, extra_paths, ws):
         _script_template = zc.buildout.easy_install.script_template
         zc.buildout.easy_install.script_template = auf_buildout_file
         project = self.options.get('projectegg', self.options['project'])
-        scripts =  zc.buildout.easy_install.scripts(
+        scripts = zc.buildout.easy_install.scripts(
             [(self.options.get('control-script', self.name),
               'auf.recipe.django.manage', 'main')],
             ws, self.options['executable'], self.options['bin-directory'],
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 name = 'auf.recipe.django'
-version = '1.7'
+version = '2.0'
 
 setup(name=name,
       version=version,
-      description="Recette Django AUF avec nos pratiques internes",
+      description="Recette Django AUF",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
       keywords='',
       author='Olivier Larchev\xc3\xaaque',
       author_email='olivier.larcheveque@auf.org',
       url='http://pypi.auf.org/%s' % name,
       license='GPL',
-      namespace_packages = ['auf', 'auf.recipe', ],
+      namespace_packages=['auf', 'auf.recipe', ],
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
